b0c3dad2268d146019326474155b4df1c3560679
LizzTebbutt <lizztebbutt@hotmail.co.uk>
2014-08-19 12:10:16 +0100

    Running logo/waterwheel.py

--- logo/waterwheel.py
+++ logo/waterwheel.py
@@ -1,8 +1,8 @@
 from tealight.logo import move, turn
 
 
-def polygon(side):
-  for i in range(0,4):
+def square(side):
+  for i in range(0,3):
     move(side)
     turn(90)
 
@@ -11,8 +11,8 @@ def waterwheel(edges, size):
   decoration = size / 2
   for i in range(0, edges):
     move(size)
-    polygon(decoration)
+    square(decoration)
     turn(angle)
 
-turn(-90)
-waterwheel(20,75)
+turn(-90)                                 #rotates around central pivot
+waterwheel(20,75)                         #size of waterwheel
