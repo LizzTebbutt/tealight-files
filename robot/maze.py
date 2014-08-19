ff2857949ad19e0f5f8f02bc202bc0469b1e4c4d
LizzTebbutt <lizztebbutt@hotmail.co.uk>
2014-08-19 14:43:37 +0100

    Running robot/maze.py

--- robot/maze.py
+++ robot/maze.py
@@ -6,7 +6,11 @@ from tealight.robot import (move,
                             left_side, 
                             right_side)
 
-If left_side != wall
+If left_side != "wall"
   turn 1
   move
+Elseif right_side != "wall" 
+  turn -1
+  move
+  
   
