e243b0ec406a6ca54402dec826ec3040cce9dc4a
LizzTebbutt <lizztebbutt@hotmail.co.uk>
2014-08-19 14:53:26 +0100

    Running robot/maze.py

--- robot/maze.py
+++ robot/maze.py
@@ -6,11 +6,10 @@ from tealight.robot import (move,
                             left_side, 
                             right_side)
 
-if left_side() != "wall":
-  turn(1)
+#move forwards until you can't move any more
+#if a wall is hit, check the left side for a wall
+#if there is a wall to the left, turn left and check again
+#move again
+
+while touch() !="wall":
   move
-else if right_side() != "wall": 
-  turn(-1)
-  move
-  
-  
