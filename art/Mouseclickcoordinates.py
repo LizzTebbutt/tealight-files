619155f3265b181bc6c40e8b1f3b92e7304ec1ee
LizzTebbutt <lizztebbutt@hotmail.co.uk>
2014-08-21 11:00:38 +0100

    Running art/Mouseclickcoordinates.py

--- art/Mouseclickcoordinates.py
+++ art/Mouseclickcoordinates.py
@@ -1,7 +1,19 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
+import github.davidsamueljones.art.Minesweeper
 
+def handle_mousemove(x,y,button):
+  
+  global lastx, lasty
+  
   if button == "left":
     color("blue")
     line(lastx, lasty, x, y)
     lastx = x
     lasty = y
+    
+  if button == "right":
+    color("red")
+    line(lastx, lasty, x, y)
+    lastx = x
+    lasty = y
+    
