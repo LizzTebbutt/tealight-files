e95b6539bdc4f84b726a932a8d47d6f17deb51c0
LizzTebbutt <lizztebbutt@hotmail.co.uk>
2014-08-21 11:35:03 +0100

    Running art/Mouseclickcoordinates.py

--- art/Mouseclickcoordinates.py
+++ art/Mouseclickcoordinates.py
@@ -71,7 +71,7 @@ def handle_mousedown(Mx,My):
 def handle_mousemove(Mx, My, button):
   global lastx, lasty
   
-  if 0 < Mx < 50:
+  if 50 < Mx < 550 & 50 < My < 550:
     lastx = Mx
     lasty = My
     print(Mx)
