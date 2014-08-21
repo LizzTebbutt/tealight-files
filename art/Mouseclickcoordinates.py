d28b52ca1484485c18c9c7ea8e4ff286dccde220
LizzTebbutt <lizztebbutt@hotmail.co.uk>
2014-08-21 10:58:09 +0100

    Running art/Mouseclickcoordinates.py

--- art/Mouseclickcoordinates.py
+++ art/Mouseclickcoordinates.py
@@ -1,2 +1,7 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 
+  if button == "left":
+    color("blue")
+    line(lastx, lasty, x, y)
+    lastx = x
+    lasty = y
