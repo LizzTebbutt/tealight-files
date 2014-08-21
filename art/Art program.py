74cd2267fdc6a155f21085a21c2e41d42ddf4bb1
LizzTebbutt <lizztebbutt@hotmail.co.uk>
2014-08-21 14:25:39 +0100

    Running art/Art program.py

--- art/Art program.py
+++ art/Art program.py
@@ -3,4 +3,4 @@ from tealight.art import (color, line, spot, circle, box, image, text, backgroun
 #---------------------------------colour pallete---------------------------------#
 
 box(20,20,30,30)
-box(51,51,30,30)
+box(51,20,30,30)
